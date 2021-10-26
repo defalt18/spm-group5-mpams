138dfdb462239116c7c7484246112de668b59fd6
Mahir Ratanpara <201801004@daiict.ac.in>
2021-10-26 10:03:48 +0530

    starter

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="bg-gray-900 text-white h-screen w-screen grid place-items-center">
+      <div>
+        <p className="text-4xl font-bold">Multi-Profession Appointment Management System</p>
+        <a className="text-base text-center mt-5" href="//localhost:5000">
+          Backend running at port 5000
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
